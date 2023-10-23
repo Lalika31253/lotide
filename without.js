ee49e991696e382b717fa770b5e8246f0c3547c9
Lalika31253 <Lalika31253@gmail.com>
2023-10-22 22:49:57 -0600

    Add some test cases

--- without.js
+++ without.js
@@ -34,3 +34,6 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+assertArraysEqual(without([2022, 2023, 2024], [2023]), [2022, 2024]); // => should PASS
+assertArraysEqual(without(["Jun", "July", "Monday", 10], ["Monday", "10"]), ["Jun", "July"]); // => should FAIL

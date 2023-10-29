9c2b26944e0ab924ce3e04c4f48015d6cf6c0bcf
Lalika31253 <Lalika31253@gmail.com>
2023-10-28 22:29:07 -0600

    Implement the function findKey

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Faild: ${actual} !== ${expected}`);
+  }
+}
+
+const findKey = function (obj, callback) {
+
+  for (let item in obj) {
+    if (callback(obj[item])) {
+      return item;
+    }
+  }
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 5) // => "noma"
+
+assertEqual(result, "noma");
+assertEqual(result1, undefined);
+
+
